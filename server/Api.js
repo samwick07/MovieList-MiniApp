0ef2322cfce6de0e43ae6a6676694c49628931e9
samwick07 <samwick07@gmail.com>
2023-09-08 07:24:22 -1000

    Express server root route functional.

--- server/Api.js
+++ server/Api.js
@@ -1,1 +1,7 @@
 const express = require('express');
+const api = express();
+const port = 8080;
+
+api.get('/', (req, res) => res.send(`Localhost Movie List API is listening at http://localhost:${port}!`));
+
+api.listen(port, () => console.log(`Localhost Movie List API is listening at http://localhost:${port}!`));
